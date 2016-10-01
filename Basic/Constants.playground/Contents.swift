370832faaefffa873eee40ae5ed0f6403db22ee7
Weibin LUO <Amabel@Weibins-MacBook-Pro.local>
2016-10-01 21:32:53 +0900

    Constants

---
+++ Basic/Constants.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let constA = 42
+print(constA)
+
+
+let constB : Float = 3.14159
+print(constB)
+
+
+let _const = "Hello, Swift"
+print(_const)
+
+let name = "baidu"
+let site = "http://www.baidu.comm"
+print("\(name) = \(site)")
