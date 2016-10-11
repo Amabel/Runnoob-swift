36f2390dfda111e846072152cdd3f6ac116d5ea4
Weibin <Amabel@Weibins-MacBook-Pro.local>
2016-10-12 01:45:22 +0900

    Pinch gesture

--- FaceIt/FaceIt/FaceViewController.swift
+++ FaceIt/FaceIt/FaceViewController.swift
@@ -16,10 +16,10 @@ class FaceViewController: UIViewController {
         }
     }
     
+
     @IBOutlet weak var faceView: FaceView! {
         didSet {
-
-            updateUI()
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: #selector(FaceView.changeScale(_:))))
         }
     }
     
@@ -35,7 +35,7 @@ class FaceViewController: UIViewController {
     
     private var mouthCurvatures = [FacialExpression.Mouth.Frown:-1.0,.Grin:0.5,.Smile:1.0,.Smirk:-0.5,.Neutral:0.0 ]
     private var eyeBrowTilts = [FacialExpression.EyeBrows.Relaxed:0.5,.Furrowed:-0.5,.Normal:0.0]
-   
+    
     func increaseHappiness() {
         expression.mouth = expression.mouth.happierMouth()
     }
@@ -68,8 +68,8 @@ class FaceViewController: UIViewController {
             break
         }
     }
-
-
-
+    
+    
+    
 }
 
