c191b991df7a2a9d67c115e88c49fbf023069775
Amabel <luoweibinb@gmail.com>
2017-02-21 21:55:29 +0900

    Login screen

---
+++ Login/Login/LoginTextField.swift
@@ -0,0 +1,30 @@
+//
+//  LoginTextField.swift
+//  Login
+//
+//  Created by Weibin on 2/21/17.
+//  Copyright © 2017 Weibin Luo. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class LoginTextField: UITextField {
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        self.layer.borderColor = UIColor(white: 231 / 255, alpha: 1).cgColor
+        self.layer.borderWidth = 1
+        
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 8, dy: 7)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return textRect(forBounds: bounds)
+    }
+
+}
